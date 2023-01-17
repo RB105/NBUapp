eaa4195d9a2f08d5f1ae192cb6a814857195fd80
RB105 <01baxromrajabov@gmail.com>
2023-01-17 16:48:14 +0500

    search function activated

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:currencyapp/data/repository/repository.dart';
 import 'package:currencyapp/provider/home_provider.dart';
+import 'package:currencyapp/view/screens/search_delegate.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:provider/provider.dart';
@@ -26,34 +27,85 @@ class _HomePageState extends State<HomePage> {
 
   Scaffold _scaffold() {
     return Scaffold(
-      
       body: SafeArea(
         child: Column(
           children: [
             Expanded(
-              flex: 2,
-              child: Container(
-              height: double.infinity,
-              width: double.infinity,
-              color: Colors.amber,)),
+                flex: 3,
+                child: Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Colors.blueGrey.shade900,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ListTile(
+                        leading: const CircleAvatar(),
+                        title: const Text("Welcome Back",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.grey),),
+                        subtitle: const Text("Name",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.white),),
+                        trailing: SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.25,
+                          height: 30,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                      Icons.qr_code_scanner_outlined,color: Colors.grey,size: 30,)),
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.wallet,color: Colors.grey,size: 30,))
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )),
             const Divider(),
+            ElevatedButton(
+                onPressed: () {
+                  showSearch(
+                      context: context,
+                      delegate: MySearchDelegate(
+                          box: RepositoryCurrency.currencyBox!));
+                },
+                style: ElevatedButton.styleFrom(
+                    shape: const StadiumBorder(),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 70, vertical: 10),
+                    textStyle: const TextStyle(fontSize: 20)),
+                child: const Text("Search")),
             Expanded(
               flex: 8,
-              child: Builder(builder: (context) {
-                if (context.watch<HomeProvider>().isLoading) {
-                  return const Center(child: CircularProgressIndicator(),);
-                }else if(context.watch<HomeProvider>().error.isNotEmpty){
-                  String bug=context.watch<HomeProvider>().error;
-                  return Center(child: Text(bug.toString()),);
-                } else {
-                  Box<CurrencyModel> data=RepositoryCurrency.currencyBox!;
-                  return ListView.builder(
-                    itemCount: data.length,
-                    itemBuilder: (context, index) {
-                    return ListTile(title: Text(data.getAt(index)!.title.toString()),);
-                  },);
-                }
-              },),
+              child: Builder(
+                builder: (context) {
+                  if (context.watch<HomeProvider>().isLoading) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (context.watch<HomeProvider>().error.isNotEmpty) {
+                    String bug = context.watch<HomeProvider>().error;
+                    return Center(
+                      child: Text(bug.toString()),
+                    );
+                  } else {
+                    Box<CurrencyModel> data = RepositoryCurrency.currencyBox!;
+                    return ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(data.getAt(index)!.title.toString()),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
             ),
           ],
         ),
---
+++ lib/view/screens/search_delegate.dart
@@ -0,0 +1,60 @@
+import 'package:currencyapp/data/model/model.dart';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
+class MySearchDelegate extends SearchDelegate {
+  final Box<CurrencyModel> box;
+
+  MySearchDelegate({required this.box});
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            if (query.isEmpty) {
+              close(context, null);
+            } else {
+              query = '';
+            }
+          },
+          icon: const Icon(Icons.clear_rounded))
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          close(context, null);
+        },
+        icon: const Icon(Icons.arrow_back));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<CurrencyModel> results = box.values.where((element) {
+      return element.title!.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+    return _listView(results);
+  }
+
+  ListView _listView(List<CurrencyModel> results) {
+    return ListView.builder(
+      itemCount: results.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(results[index].title.toString()),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<CurrencyModel> suggestions = box.values.where((element) {
+      return element.title!.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+    return _listView(suggestions);
+  }
+}
